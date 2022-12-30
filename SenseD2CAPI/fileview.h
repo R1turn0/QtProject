8707d000beefed64a57a450957974dab0bd84088
R1turn0 <714669315@qq.com>
2022-12-30 11:30:00 +0800

    Update

--- SenseD2CAPI/fileview.h
+++ SenseD2CAPI/fileview.h
@@ -2,8 +2,20 @@
 #define FILEVIEW_H
 
 #include <QWidget>
-#include <QFileDialog>
+#include <QDebug>
 #include <QMessageBox>
+#include <ctype.h>
+#include <QDateTime>
+#include <QFileDialog>
+#include <time.h>
+
+#include <inc/d2c.h>
+#include <inc/ss_error.h>
+#include <inc/ss_lm_runtime.h>
+#include <inc/ss_lm_control.h>
+#include <common/ss_help.h>
+
+#define MAX_PATH 260
 
 namespace Ui {
 class FileView;
@@ -17,6 +29,12 @@ public:
     explicit FileView(QWidget *parent = nullptr);
     ~FileView();
 
+    int make_reset_d2c_to_file(MASTER_HANDLE hmaster, SS_BYTE* root_ca_cert, SS_UINT32 root_ca_cert_len);
+
+    int make_d2c_to_file(D2C_HANDLE d2c_handle, char* filename_prefix);
+
+    int create_JSON();
+
 private slots:
     void on_licenseButton_clicked();
 
@@ -24,15 +42,26 @@ private slots:
 
     void on_cancelButton_clicked();
 
-    void on_operationTypeComboBox_activated(int index);
+    void on_signButton_clicked();
 
-    void on_fileTypeComboBox_activated(int index);
+    void on_operationTypeComboBox_currentTextChanged(const QString &arg1);
 
-    void on_fileDownloadButton_clicked();
+    void on_fileTypeComboBox_currentTextChanged(const QString &arg1);
+
+    void on_fileOffsetLineEdit_textChanged(const QString &arg1);
+
+    void on_licenseIDLineEdit_textChanged(const QString &arg1);
 
 private:
     Ui::FileView *ui;
-    QString fileName;
+    SS_CHAR* result;
+    char operationType[12];
+    char timeStamp[11];
+    char fileType[4];
+    char fileName[256];
+    char fileBuffer[256];
+    QString fileOffset;
+    QString bindLic;
 };
 
 #endif // FILEVIEW_H
